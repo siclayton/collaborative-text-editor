31f499a87ef653f9d44ea37275947d716738e8fb
Simon <clayto23@lancaster.ac.uk>
2026-07-22 17:23:09 +0100

    Added ability to get documents

--- backend/src/main/java/org/example/backend/document/DocumentController.java
+++ backend/src/main/java/org/example/backend/document/DocumentController.java
@@ -1,6 +1,7 @@
 package org.example.backend.document;
 
 import org.springframework.web.bind.annotation.*;
+import java.util.List;
 
 @RestController
 @RequestMapping("/documents")
@@ -13,5 +14,13 @@ public class DocumentController {
     @PostMapping
     public Document createDocument(@RequestBody Document document) {
         return documentRepository.save(document);
+    }
+    @GetMapping
+    public List<Document> getAllDocuments() {
+        return documentRepository.findAll();
+    }
+    @GetMapping("/{id}")
+    public Document getDocumentById(@PathVariable Long id) {
+        return documentRepository.findById(id).orElseThrow();
     }
 }
